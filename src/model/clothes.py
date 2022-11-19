320860e6688fb5639047da36bbbf284ff39d0325
Remmian <remixzapata@gmail.com>
2022-11-19 17:39:02 -0500

    src directory added

---
+++ src/model/clothes.py
@@ -0,0 +1,2 @@
+clothesAvailable = ["Jeans", "Faldas", "Shorts", "Tops", "Casacas"]
+sizesAvailable = ["S", "M", "L", "XL", "XXL"]
---
+++ src/plugins/widgets.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import ttk
+import os
+
+
+class Widgets:
+    def __init__(self, root):
+        self.master = root
+        # Ruta del proyecto
+        self.home = os.getcwd()
+        # Fuente de los widgets
+        self.fontOptions = ('Helvetica', 14, 'normal')
+        # ttk color theme
+        # Define the style for combobox widget
+        style= ttk.Style()
+        style.theme_use('clam')
+        style.configure("TCombobox", fieldbackground= "orange", background= "white") 
+
+    def label(self, text):
+        label = Label(text=text)
+        return label
+
+    def comboBox(self, options):
+        combo = ttk.Combobox(
+            state="readonly",
+            values=options,
+        )
+        combo.set("Seleccione una opción")
+        return combo
+
+    def button(self, text, callback):
+        button = Button(self.master, text=text, font=self.fontOptions,
+                        bg="black", fg="white", command=callback)
+        return button
+
+    def scale(self, min, max, orient):
+        scale = Scale(self.master, from_=min, to=max,
+                      bg="#fea75d", fg="black", orient=orient)
+        return scale
+
+    # def setBackgroundImage(self, imageRoute):
+    #     print(imageRoute)
+    #     imagen = PhotoImage(file=imageRoute)
+    #     background = Label(image=imagen)
+    #     background.place(x=0, y=0, relwidth=1, relheight=1)
+    #     return background
